fe3cdc1108ac388e0732c9b35ff6e2869197f592
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-03-21 15:44:51 -0300

    Add dotenv - Protect my secret data in .env file

--- app/controllers/confesses_controller.rb
+++ app/controllers/confesses_controller.rb
@@ -1,7 +1,25 @@
 class ConfessesController < ApplicationController
-  def new
-  end
 
   def create
+    @confess = current_user.confesss.build(confess_params)
+
+    if @confess.save!
+      ConfessMailer.creation_confirmation(@confess).deliver_now
+      if @confess.email.nil? ==  false
+      	flash[:notice] = "Parabéns, você desabafou! Enviamos uma dica legal para o seu e-mail";      	
+      else
+      	flash[:notice] = "Parabéns, você desabafou!";      	
+
+      end
+    else
+      flash[:alert] = "Você não conseguiu enviar o seu desabafo, tente novamente";
+    end
+    redirect_to root
+  end
+
+  private
+
+  def confess_params
+  	params.require(:confess).permit(:name, :email, :text)
   end
 end
